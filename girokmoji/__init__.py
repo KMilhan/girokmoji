320584759cba3a4f52807a0f5e630770a5226765
semantic-release <semantic-release>
2025-06-28 02:59:09 +0000

    0.5.15
    Automatically generated by python-semantic-release

--- girokmoji/__init__.py
+++ girokmoji/__init__.py
@@ -9,6 +9,6 @@ from .release import auto_release as auto_release
 try:  # pragma: no cover - package might not be installed in tests
     __version__ = metadata.version(__package__ or "girokmoji")
 except metadata.PackageNotFoundError:  # pragma: no cover - fallback version
-    __version__ = "0.5.14"
+    __version__ = "0.5.15"
 
 __all__ = ["change_log", "github_release_payload", "auto_release", "__version__"]
